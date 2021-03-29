0af62732e17fd29c3a564dfc4be200f67f653c2c
jhaaso <justin.haas@cuw.edu>
2021-03-29 11:57:38 -0500

    updated for hw

--- app/src/main/java/com/example/csc250_diceroller/MainActivity.java
+++ app/src/main/java/com/example/csc250_diceroller/MainActivity.java
@@ -4,11 +4,78 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button zeroButton;
+    private Button oneButton;
+    private Button twoButton;
+    private Button threeButton;
+    private Button fourButton;
+    private Button fiveButton;
+    private Button sixButton;
+    private Button sevenButton;
+    private Button eightButton;
+    private Button nineButton;
+    private TextView qtyTV;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        //this.oneButton = this.find(ViewById(R.id.Oneb));
+        //this.qtyTV = this.find(ViewById(R.id.qtyTV));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.qtyTV = this.findViewById(R.id.qtyTV);
+        this.qtyTV.setText("");
+        this.currentQtyText = "";
     }
+
+    public void diceButtonPressed(View v)
+    {
+        System.out.println("*****" + v.getId());
+    }
+
+    public void qtyButtonPressed(View v)
+    {
+
+        Button b = (Button)v;
+
+        if(this.QtyText.length() == 0 && b.getText().equals("0"))
+        {
+            return;
+        }
+        this.currentQtyText += b.getText();
+
+    }
+    private void extractNumberOfSides(String diceType)
+    {
+        String answer = "";
+        for (int i = i; i < diceType.length(); i++)
+        {
+            answer += diceType.charAt(i);
+            {
+                return answer
+            }
+        }
+
+    }
+
+    public void clearButtonPressed(View v)
+    {
+        this.currentQtyText = "";
+        this.qtyTV.setText(this.currentQtyText);
+    }
+    public void qtyButtonPressed(View v)
+    {
+        Button B = (Button) v;
+        this.qtyTV.setText(b.getText());
+
+        if ()
+        this.currentlyQtyText += b.getText();
+        this.qtyTV.setText(this.CurrentQtyText);
+    }
+
 }
